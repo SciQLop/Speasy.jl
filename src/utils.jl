bf1d6c67d3340c9f1b0bee80f004057fe7bf6ef0
Beforerr <zzj956959688@gmail.com>
2025-01-27 09:26:15 -0800

    fix: temporary time conversion

--- src/utils.jl
+++ src/utils.jl
@@ -9,8 +9,8 @@ function pyconvert_time(time)
     pydt_min = @pyconst(pyimport("numpy").timedelta64(1, "ns"))
     dt_min = Nanosecond(1)
     pyt0 = time[0]
-    # t0 = pyconvert(DateTime, pyt0.astype("datetime64[us]").item())
-    t0 = pyconvert(DateTime, pyt0)
+    t0 = pyconvert(DateTime, pyt0.astype("datetime64[s]").item()) # temporary solution, related to https://github.com/JuliaPy/PythonCall.jl/pull/509
+    # t0 = pyconvert(DateTime, pyt0)
     dt_f = pyconvert(Array, (time - pyt0) / pydt_min)
     return t0 .+ dt_f .* dt_min
 end
