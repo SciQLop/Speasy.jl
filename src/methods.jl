1632296ae1889393e30de2ca6296dc9d9fda1693
Beforerr <zzj956959688@gmail.com>
2025-01-28 10:25:22 -0800

    fix: return SpeasyVariable

--- src/methods.jl
+++ src/methods.jl
@@ -1,4 +1,4 @@
 replace_fillval_by_nan(var) = SpeasyVariable(var.py.replace_fillval_by_nan())
-replace_fillval_by_nan!(var) = var.py.replace_fillval_by_nan(inplace=true)
-sanitize!(var; kwargs...) = var.py.sanitized(; inplace=true, kwargs...)
+replace_fillval_by_nan!(var) = (var.py.replace_fillval_by_nan(inplace=true); var)
+sanitize!(var; kwargs...) = (var.py.sanitized(; inplace=true, kwargs...); var)
 sanitize(var; kwargs...) = SpeasyVariable(var.py.sanitized(; kwargs...))
