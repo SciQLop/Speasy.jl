483e423d794156e1d51c04459ffa1402ef4bb36f
Beforerr <zzj956959688@gmail.com>
2025-02-19 15:23:45 -0800

    feat: init

--- src/Speasy.jl
+++ src/Speasy.jl
@@ -11,6 +11,7 @@ export replace_fillval_by_nan, replace_fillval_by_nan!, sanitize, sanitize!
 export isspectrogram
 export speasyplot, speasyplot!
 export DataSet
+export init_amda, init_cdaweb, init_csa, init_sscweb, init_archive, init_providers
 
 include("utils.jl")
 include("types.jl")
@@ -23,6 +24,13 @@ function get_data(args...)
     res = @pyconst(pyimport("speasy").get_data)(args...)
     return apply_recursively(res, SpeasyVariable, is_pylist)
 end
+
+init_amda() = @pyconst(pyimport("speasy.core.requests_scheduling.request_dispatch").init_amda)(ignore_disabled_status=true)
+init_cdaweb() = @pyconst(pyimport("speasy.core.requests_scheduling.request_dispatch").init_cdaweb)(ignore_disabled_status=true)
+init_csa() = @pyconst(pyimport("speasy.core.requests_scheduling.request_dispatch").init_csa)(ignore_disabled_status=true)
+init_sscweb() = @pyconst(pyimport("speasy.core.requests_scheduling.request_dispatch").init_sscweb)(ignore_disabled_status=true)
+init_archive() = @pyconst(pyimport("speasy.core.requests_scheduling.request_dispatch").init_archive)(ignore_disabled_status=true)
+init_providers() = @pyconst(pyimport("speasy.core.requests_scheduling.request_dispatch").init_providers)(ignore_disabled_status=true)
 
 function speasyplot end
 function speasyplot! end
